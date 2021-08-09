6581e448dddcb56fb1fa4761ac06fd988a5e4942
Nehal <nehal@Apples-MacBook-Pro.local>
2021-08-09 22:24:10 +0530

    whatsapp nodejs backend template

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,29 @@
+const register = (req, res) => {
+    //register api logic here
+};
+
+const addContact = (req, res) => {
+    //addContact api logic here
+};
+
+const getContactsList = (req, res) => {
+    //getContactsList api logic here
+};
+
+const sendMessage = (req, res) => {
+    //sendMessage api logic here
+};
+
+const retreiveChat = (req, res) => {
+    //retreiveChat api logic here
+};
+
+const UserController = {
+    register,
+    addContact,
+    getContactsList,
+    sendMessage,
+    retreiveChat
+};
+
+module.exports = UserController;
